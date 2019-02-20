478b7672c0825e4f8a4c87630368dde8f737784c
totechite <yutan.h9@gmail.com>
2019-02-21 00:30:14 +0900

    fix

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,48 +9,65 @@ pub struct Lexer {
 }
 
 impl Lexer {
-
-    pub fn new(src: String) -> Lexer{
+    pub fn new(src: String) -> Lexer {
         let mut cursor = Cursor::new(src);
         let mut buf = String::new();
         cursor.read_line(&mut buf);
-        Lexer{
+        Lexer {
             cursor: cursor,
-            line: Some(buf)
+            line: Some(buf),
         }
     }
 
-    fn consume(&mut self) -> &mut Lexer{
-            let mut temp: String = "".to_string();
-            self.cursor.read_line(&mut temp);
-            match temp.as_str(){
-                "" => self.line = None,
-                x => self.line = Some(x.to_string())
-            }
-            self
+    fn consume(&mut self) -> &mut Lexer {
+        let mut temp: String = "".to_string();
+        self.cursor.read_line(&mut temp);
+        match temp.as_str() {
+            "" => self.line = None,
+            x => self.line = Some(x.to_string()),
+        }
+        self
     }
-    
-    pub fn exec(&mut self) -> Vec<Token>{
+
+    pub fn exec(&mut self) -> Vec<Token> {
         let mut tokens = vec![];
-        while let Some(head_line) = self.line.clone(){
-            if "\n" == head_line.as_str(){
+        while let Some(head_line) = self.line.clone() {
+            if "\n" == head_line.as_str() {
                 // Nothing to do
                 self.consume();
             }
             // Heading
-            else if Regex::new(r"^(#{1,6})\s").unwrap().is_match(head_line.as_str()) {
-                let depth: i8 = Regex::new("#").unwrap().find_iter(head_line.as_str()).count() as i8;
-                let text: String = Regex::new("#").unwrap().replace_all(head_line.as_str(), "").trim().to_string();
-                tokens.push(Token::Heading{depth: depth,text: text});
+            else if Regex::new(r"^(#{1,6})\s")
+                .unwrap()
+                .is_match(head_line.as_str())
+            {
+                let depth: i8 = Regex::new("#")
+                    .unwrap()
+                    .find_iter(head_line.as_str())
+                    .count() as i8;
+                let text: String = Regex::new("#")
+                    .unwrap()
+                    .replace_all(head_line.as_str(), "")
+                    .trim()
+                    .to_string();
+                tokens.push(Token::Heading {
+                    depth: depth,
+                    text: text,
+                });
                 self.consume();
             }
             // Code
-            else if Regex::new(r"^\B[(`{3})(\t{1})]").unwrap().is_match(head_line.as_str()) {
-                let mut find_lang: Option<_> = Regex::new(r"[\w[0-9]]+").unwrap().captures(head_line.as_str());
+            else if Regex::new(r"^\B[(`{3})(\t{1})]")
+                .unwrap()
+                .is_match(head_line.as_str())
+            {
+                let mut find_lang: Option<_> = Regex::new(r"[\w[0-9]]+")
+                    .unwrap()
+                    .captures(head_line.as_str());
                 let mut lang: String = String::new();
-                if let Some(option) = find_lang{
+                if let Some(option) = find_lang {
                     lang = option.get(0).unwrap().as_str().to_string();
-                }else{
+                } else {
                     lang = "".to_string();
                 }
                 self.consume();
