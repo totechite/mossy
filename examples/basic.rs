a47bb01868b75b4ef80d4748772ae59c946a2bb2
totechite <yutan.h9@gmail.com>
2019-02-21 00:23:34 +0900

    add example

---
+++ examples/basic.rs
@@ -0,0 +1,23 @@
+extern crate mossy;
+
+use mossy::Mossy;
+
+fn main() {
+    let markdown_text = r"
+mossy
+================
+A toy library for parsing Markdown.
+
+# Specification
+It's based [CommonMark].
+
+LICENSE
+----------------
+MIT
+
+[CommonMark]: https://spec.commonmark.org/0.28/
+"
+    .to_string();
+
+    println!("{}", Mossy::new(markdown_text));
+}
